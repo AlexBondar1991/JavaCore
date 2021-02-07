be8b783fa36fa19086e635a7ae2d42845a3b1a4e
Alex <nazgulqaz2@gmail.com>
2021-02-07 23:34:22 +0300

    lesson one

---
+++ MyFirstClass.java
@@ -0,0 +1,11 @@
+package com.company.lesson_first;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class MyFirstClass {
+    public static void main(String[] args) throws FileNotFoundException {
+
+    }
+}
---
+++ Types.java
@@ -0,0 +1,17 @@
+package com.company.lesson_first;
+
+public class Types {
+    public static void main(String[] args) {
+        byte min = -128;
+        byte max = 127;
+        short minShort = Short.MIN_VALUE;
+        short maxShort = Short.MAX_VALUE;
+        int minInt = Integer.MIN_VALUE;
+        int maxInt = Integer.MAX_VALUE;
+        long minLong = Long.MIN_VALUE;
+        long maxLong = Long.MAX_VALUE;
+
+        System.out.println(maxLong);
+    }
+
+}
